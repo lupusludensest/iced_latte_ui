018f94e339b19e693f376a626af4b398e50c6d36
Vic Gurov <gurovvic@gmail.com>
2024-03-22 15:44:56 -0400

    dt 22 march 2024

--- TDD/README.md
+++ TDD/README.md
@@ -1,3 +1,5 @@
-pytest --alluredir=allure_reports -k "test_refactored_1.py or test_refactored_2.py or test_refactored_3.py or test_refactored_4.py"
+# run all tests refactored
+pytest --alluredir=allure_reports_refactored -k "test_refactored_1.py or test_refactored_2.py or test_refactored_3.py or test_refactored_4.py"
 
-allure serve allure_reports
+# allure report
+allure serve allure_reports_refactored
--- TDD/allure_tdd_run.bat
+++ TDD/allure_tdd_run.bat
@@ -1,6 +1,6 @@
-pytest --alluredir=’E:\Gurov_SSD_256\IT\Testing\Automation_08_09_2019\iced_latte_ui\TDD\allure_tdd_reports’
-pytest --alluredir=’E:\Gurov_SSD_256\IT\Testing\Automation_08_09_2019\iced_latte_ui\TDD\allure_tdd_reports\’ test_iced_latte_ui_register_new_user_1.py
-pytest --alluredir=’E:\Gurov_SSD_256\IT\Testing\Automation_08_09_2019\iced_latte_ui\TDD\allure_tdd_reports\’ test_iced_latte_ui_register_user_logged_in_2.py
+pytest --alluredir=’E:\Gurov_SSD_256\IT\Testing\Automation_08_09_2019\iced_latte_ui\TDD\allure_tdd_reports --clean-alluredir’
+pytest --alluredir=’E:\Gurov_SSD_256\IT\Testing\Automation_08_09_2019\iced_latte_ui\TDD\allure_tdd_reports\ --clean-alluredir’ test_1_iced_latte_ui_register_new_user.py
+pytest --alluredir=’E:\Gurov_SSD_256\IT\Testing\Automation_08_09_2019\iced_latte_ui\TDD\allure_tdd_reports\ --clean-alluredir’ test_2_iced_latte_ui_registered_user_logged_in.py
 
 
 
