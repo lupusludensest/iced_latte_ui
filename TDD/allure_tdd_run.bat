f26c3ca6ebb6f065ff1138e248265b27a9f90f57
Vic Gurov <gurovvic@gmail.com>
2024-03-31 20:05:06 -0400

    dt 31 march 2024

--- TDD/allure_tdd_run.bat
+++ TDD/allure_tdd_run.bat
@@ -1,6 +1,6 @@
-pytest --alluredir=’E:\Gurov_SSD_256\IT\Testing\Automation_08_09_2019\iced_latte_ui\TDD\allure_tdd_reports’
-pytest --alluredir=’E:\Gurov_SSD_256\IT\Testing\Automation_08_09_2019\iced_latte_ui\TDD\allure_tdd_reports\’ test_1_iced_latte_ui_register_new_user.py
-pytest --alluredir=’E:\Gurov_SSD_256\IT\Testing\Automation_08_09_2019\iced_latte_ui\TDD\allure_tdd_reports\ ’ test_2_iced_latte_ui_registered_user_logged_in.py
+pytest --alluredir=’E:\Gurov_SSD_256\IT\Testing\Automation_08_09_2019\iced_latte_ui_tdd\TDD\allure_tdd_reports’
+pytest --alluredir=’E:\Gurov_SSD_256\IT\Testing\Automation_08_09_2019\iced_latte_ui_tdd\TDD\allure_tdd_reports\’ test_1_iced_latte_ui_register_new_user.py
+pytest --alluredir=’E:\Gurov_SSD_256\IT\Testing\Automation_08_09_2019\iced_latte_ui_tdd\TDD\allure_tdd_reports\ ’ test_2_iced_latte_ui_registered_user_logged_in.py
 
 
 
