136086178de67bfb890e09698573a931fc7814f1
Vic Gurov <gurovvic@gmail.com>
2024-03-31 21:02:52 -0400

    31 march 2024 added iced_latte_ui_tdd

--- TDD/allure_tdd_report.bat
+++ TDD/allure_tdd_report.bat
@@ -1,1 +1,1 @@
-allure serve E:\Gurov_SSD_256\IT\Testing\Automation_08_09_2019\iced_latte_ui_tdd\TDD\allure_tdd_reports
+allure serve E:\Gurov_SSD_256\IT\Testing\Automation_08_09_2019\Iced-Latte-QA\iced_latte_ui_tdd\TDD\allure_tdd_reports
